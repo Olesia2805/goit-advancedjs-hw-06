586db0baedb4b2bb74734ed0835b3f1f73fe474a
Olesia2805 <vysotina2002@gmail.com>
2024-09-08 00:42:12 +0200

    bT task 9th completed

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,23 +1,42 @@
-// /*
-//   Створіть новий тип даних, який підходить для цих двох об'єктів.
-// */
+/*
+  Create a new data type that is suitable for these two objects.
+*/
 
-// const page1 = {
-//   title: 'The awesome page',
-//   likes: 100,
-//   accounts: ['Max', 'Anton', 'Nikita'],
-//   status: 'open',
-//   details: {
-//     createAt: new Date('2021-01-01'),
-//     updateAt: new Date('2021-05-01'),
-//   }
-// }
+type LessDetails = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: string,
+}
 
-// const page2 = {
-//   title: 'Python or Js',
-//   likes: 5,
-//   accounts: ['Alex'],
-//   status: 'close',
-// }
+type AddDetails = {
+  details: {
+    createAt: Date,
+    updateAt: Date
+  }
+}
 
-// export {};
+type MoreDetails = LessDetails & AddDetails;
+
+const page1: MoreDetails = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+const page2: LessDetails = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
+
+export { };
+
+// console.log(page1);
+// console.log(page2);
