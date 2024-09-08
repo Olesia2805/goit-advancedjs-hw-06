16e5ff1cb333a323fca3d3117110161b01712ab1
Olesia2805 <vysotina2002@gmail.com>
2024-09-08 09:16:03 +0200

    g task 1st done

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,17 +1,18 @@
-// /*
-//   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
-//   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
-// */
+/*
+  There is a function getPromise() that returns a promise that is allowed in an array containing strings and numbers.
+  Extend this function using generics so that it returns the correct type.
+*/
 
-// function getPromise () {
-//   return new Promise((resolve) => {
-//     resolve(['Text', 50]);
-//   });
-// }
+function getPromise(): Promise<(string | number)[]> {
+    const promise: Promise<Array<string | number>> = new Promise((resolve) => {
+        resolve(['Text', 50]);
+    });
+    return promise;
+}
 
-// getPromise()
-// .then((data) => {
-//   console.log(data);
-// });
+getPromise()
+.then((data) => {
+  console.log(data);
+});
 
-// export {};
+export {};
